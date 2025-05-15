64342b70fac2ff0321c9537349791193e9712954
Gpat71 <63578389+Gpat71@users.noreply.github.com>
2025-05-15 19:34:05 +0200

    2nd file

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,3 @@
-# %%
 import pandas as pd
 from glob import glob
 
@@ -8,7 +7,9 @@ from glob import glob
 single_file_acc = pd.read_csv(
     "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv"
 )
-
+single_file_gyro = pd.read_csv(
+    "../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv"
+)
 
 # --------------------------------------------------------------
 # List all data in data/raw/MetaMotion
@@ -51,5 +52,3 @@ single_file_acc = pd.read_csv(
 # --------------------------------------------------------------
 # Export dataset
 # --------------------------------------------------------------
-
-# %%
